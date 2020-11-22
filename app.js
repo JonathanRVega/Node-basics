55825e38948b2e5900ad35e197aa1b6723bd226a
JonathanRVega <Jonathaan.rv@gmail.com>
2020-11-22 12:20:11 -0700

    finished project

--- app.js
+++ app.js
@@ -1,22 +1,64 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const { render } = require('ejs');
+const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
 
-app.listen(3000);
-app.use(morgan('dev'));
-app.use(express.static('public'));
+//connect to MongoDB
+const dbURI = 'mongodb+srv://Jonathan:Chata2001@cluster0.yisv5.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 
 app.set('view engine', 'ejs');
 
-const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+
+//mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog 2',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  });
+
+  blog.save()
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+})
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+  .then((result) => {
+    res.send(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
+
+app.get('/single-blog', (req, res) => {
+  Blog.findById('5fb9961f3917923dc4da6508')
+  .then((result) => {
+    res.send(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home', blogs});
+    res.redirect('/blogs');
   });
 
   // app.use((req, res, next) => {
@@ -37,9 +79,8 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
   });
-  app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-  });
+
+  app.use('/blogs', blogRoutes);
 
   // 404 page
   app.use((req, res) => {
